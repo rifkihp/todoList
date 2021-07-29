e6eb0b64db022c055e3c4a3f49f1d676a642d58c
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-29 22:33:41 +0530

    Update MainActivity.java

--- app/src/main/java/com/snippy/todolist/MainActivity.java
+++ app/src/main/java/com/snippy/todolist/MainActivity.java
@@ -3,9 +3,12 @@ package com.snippy.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +16,17 @@ public class MainActivity extends AppCompatActivity {
     Button add;
     ListView liview;
 
+    ArrayList<String> arli=new ArrayList<>();
+    ArrayAdapter<String> adarr;   //to connect array list and list 
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        item=findViewById(R.id.edtext);
+        add=findViewById(R.id.btn);
+        liview=findViewById(R.id.list);
     }
 }
