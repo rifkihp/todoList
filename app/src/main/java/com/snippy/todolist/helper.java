c45dc1b76ee5b2a567415b2207768aae7d15a3a5
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-30 18:31:18 +0530

    helper class to save ,read and write data

--- app/src/main/java/com/snippy/todolist/helper.java
+++ app/src/main/java/com/snippy/todolist/helper.java
@@ -2,7 +2,10 @@ package com.snippy.todolist;
 
 import android.content.Context;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
@@ -20,5 +23,20 @@ public class helper {
         catch(Exception e){
         e.printStackTrace();
         }
+    }
+
+
+    public static ArrayList<String> readData(Context ctx) {
+        ArrayList<String> itli = null;
+        try {
+            FileInputStream fis = ctx.openFileInput(filename);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            itli = (ArrayList<String>) ois.readObject();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return itli;
     }
 }
